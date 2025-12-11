c6d985a9b2bdf7479cf21a8a87b9d65f93fa746c
sineadezita <s.ezita@icloud.com>
2025-12-11 20:50:43 +0000

    Add string method to models for readability

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,15 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
+    class Meta:
+        ordering = ["-created_on"
+        ]
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
 # Comment model
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -28,3 +37,10 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+
+    class Meta:
+        ordering = ["created_on"
+        ]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
