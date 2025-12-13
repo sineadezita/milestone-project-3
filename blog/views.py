77a9bf2e50c6c252401d384bdb6ce5a7e7dde79a
sineadezita <s.ezita@icloud.com>
2025-12-13 21:52:16 +0000

    Add filter by published status to PostListView class in views

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from .models import Post
 
 
 class PostListView(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = 'blog/blog.html'
