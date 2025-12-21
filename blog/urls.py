b7a690217b78024aec0163024e31306263cfba5c
sineadezita <s.ezita@icloud.com>
2025-12-21 19:56:27 +0000

    added fashion_post view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
+    path('article/<slug:slug>/', views.fashion_post, name='fashion_post'),
 ]
