032c7d3f89e3089b2e8b9cdbd43029480460820d
sineadezita <s.ezita@icloud.com>
2025-12-28 13:34:45 +0000

    Add collaborate form and wire up for functionality

--- about/views.py
+++ about/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 def about_fashion_tech(request):
     """
     Renders About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "We have received your collaboration request. Please allow two working days for our response!")
+    
     about = About.objects.first()
-
+    collaborate_form = CollaborateForm()
+    
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
